230546b4db0fc31c7a18081988acde1e82878b6f
Luis HGO <luishgo@gmail.com>
2013-04-27 12:57:07 -0300

    Bootstrap do Aqui Tem Feira

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aquitemfeira::Application.config.secret_token = 'f6db1e863e16997991c293cc80496f2aac1f88be6bb9f799fe85ea6a5e18b037ac98f8b697833291cf657b7c2645ec56aa7869e2bfbdf3ab36aa9a6ca4e6126f'
